81594d19388db0fc76ec9f14084952a3a104619e
ergashevaziz-dev <ergashevazizdev@gmail.com>
2025-11-16 18:43:16 +0500

    updated bot

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
-TOKEN = "7698712512:AAE_mamwyM27PolwNocw7XWOfeopN_JqjNE"
-
-# Mahsulotlar kategoriyalari va ularning rasmlari
+TOKEN = "8521996170:AAF98jLq2IFsl9reOeAbUqWJIVUy3q2qyb0"
 products = {
-    "Smovor": [{"photo": "images/samovar.jpeg", "price": "80 000 so'm"}],
-    "Komplekt": [{"photo": "images/komplekt.jpg", "price": "150 000 so'm"}],
-    "Qozon": [{"photo": "images/qozon.jpeg", "price": "60 000 so'm"}],
-    "Dekorativ": [{"photo": "images/dekarativ.jpg", "price": "20 000 so'm"}],
+    "Kamplekt": [{"photo": "images/komplekt.jpg", "price": "150 000 so'm"}],
+    "Samovor": [{"photo": "images/samovar.jpeg", "price": "80 000 so'm"}],
+    "Deg": [{"photo": "images/qozon.jpeg", "price": "60 000 so'm"}],
+    "Dekarativ": [{"photo": "images/dekarativ.jpg", "price": "20 000 so'm"}],
 }
 
-# Asosiy start menyusi
 def show_start_menu(update_or_query, bot=None):
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Mahsulotlar", callback_data="categories")],
-        [InlineKeyboardButton("Manzilimiz", callback_data="show_location")],
-        [InlineKeyboardButton("Telefon", callback_data="show_phone")]
+        [InlineKeyboardButton("Mahsuloto", callback_data="categories")],
+        [InlineKeyboardButton("Lakatsiya", callback_data="show_location")],
+        [InlineKeyboardButton("Telefon nomer", callback_data="show_phone")]
     ])
 
-    # Agar /start komandasi orqali chaqirilsa
+
     if bot is not None:
-        update_or_query.reply_text("Assalomu alaykum! Bizning prokat botga xush kelibsiz!", reply_markup=keyboard)
+        update_or_query.reply_text("Assalomu alaykum! Prakat botba xush omadeton!", reply_markup=keyboard)
     else:  # Agar inline tugma orqali chaqirilsa
-        update_or_query.message.reply_text("Assalomu alaykum! Bizning prokat botga xush kelibsiz!", reply_markup=keyboard)
+        update_or_query.message.reply_text("Assalomu alaykum! Prakat botba xush omadeton!", reply_markup=keyboard)
 
 # /start handler
 def start_handler(update: Update, context: CallbackContext):
@@ -40,15 +37,15 @@ def inline_handler(update: Update, context: CallbackContext):
         keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton(cat, callback_data=f"product_{cat}")] for cat in products.keys()
         ])
-        keyboard.inline_keyboard.append([InlineKeyboardButton("Orqaga", callback_data="start")])
-        query.message.reply_text("Kategoriya tanlang:", reply_markup=keyboard)
+        keyboard.inline_keyboard.append([InlineKeyboardButton("Pushba", callback_data="start")])
+        query.message.reply_text("Kategoriyay tanla kunet:", reply_markup=keyboard)
 
     # Kategoriya bosildi -> rasm va narx
     elif data.startswith("product_"):
         category = data.split("_", 1)[1]
         product = products[category][0]
         keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Orqaga", callback_data="categories")]
+            [InlineKeyboardButton("Pushba", callback_data="categories")]
         ])
         with open(product["photo"], "rb") as photo_file:
             query.message.reply_photo(photo=photo_file,
@@ -59,17 +56,15 @@ def inline_handler(update: Update, context: CallbackContext):
     elif data == "start":
         show_start_menu(query)
 
-    # Manzil Google Maps link bilan
-    # Manzil Google Maps link bilan
     elif data == "show_location":
         latitude = 39.654  # o'zingiz latitude
         longitude = 66.959  # o'zingiz longitude
         maps_url = f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}"
 
         keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Orqaga", callback_data="start")]
+            [InlineKeyboardButton("Pushba", callback_data="start")]
         ])
-        query.message.reply_text(f"Bizning manzilimiz: [Google Maps]({maps_url})",
+        query.message.reply_text(f"Mohona Lakatsiyemo: [Google Maps]({maps_url})",
                                  parse_mode="Markdown",
                                  reply_markup=keyboard)
 
@@ -77,9 +72,9 @@ def inline_handler(update: Update, context: CallbackContext):
     elif data == "show_phone":
         phone_numbers = "+998(90)024-54-58\n+998(91)532-22-31"  # ikkita raqam yangi qatorda
         keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Orqaga", callback_data="start")]
+            [InlineKeyboardButton("Pushba", callback_data="start")]
         ])
-        query.message.reply_text(f"Bizning telefon raqamlarimiz:\n{phone_numbers}",
+        query.message.reply_text(f"Mohona telefon nomeramo:\n{phone_numbers}",
                                  reply_markup=keyboard)
 
 
